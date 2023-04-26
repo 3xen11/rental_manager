819bbc82df2e0caa985ade6627f3891aa76d4d95
Dawid Dardziński <dardzinski.d@gmail.com>
2023-04-26 21:20:25 +0200

    login, register [added]: added view for login and register feature

---
+++ src/script.js
@@ -0,0 +1,107 @@
+"use strict";
+
+class App {
+  // To co ja wynajmuję komuś
+  #myRentals = [];
+
+  constructor() {}
+
+  _addNewRental() {
+    // dodać VALUE z inputów które będą w "formularzu" dodawania lokum
+    // mieszkanie, pokój i dom powinno się wybierać z selecta i na podstawie jego VALUE budować warunki dla if'ów
+    // let rental;
+    // zrobić 3 if'y z walidacja (mieszkanie, pokój, dom)
+    // w każdym if'ie ma zostać dodany nowy wynajem:
+    // rental = new Apartament/Room/House(tutaj dane z inputów)
+    // this.#myRentals.push(rental)
+  }
+
+  _deleteRental() {
+    // każde dodane lokum ma przycisk na usunięcie, po kliknięciu w usunięcie znajduje ID które jest zaszyte w elemencie
+    // przelecieć po tavblicy #myRentals z pobranym wcześniej ID
+    // usunąć z tablicy element który zawiera to ID
+  }
+
+  _addTenantNote() {
+    //! tablica powinna być chyba w klasie lokatora
+    // stworzyć tablicę na notatki
+    // każde dodane lokum ma przycisk na dodanie notatki, po kliknięciu w dodanie znajduje ID które jest zaszyte w elemencie
+    // utwiera się popup z dodaniem notatki
+    // notatka zostaje wpushowana do tablicy obiektu który ma dane ID
+  }
+
+  _removeTenantNote() {
+    //
+  }
+
+  _addRentalNote() {
+    //! tablica powinna być chyba w klasie lokum
+  }
+
+  _removeRentalNote() {
+    //
+  }
+
+  _renderMyRental() {
+    //! jeżeli zrobię MVC to to powinno być w View
+    // renderowanie dodanych lokum
+    // trzeba zrobić tutaj markup i użyć insertAdjacentHHTML do dodania go
+  }
+
+  _calculateTotalEarning() {
+    //! nie wiem czy nie dać tego do klasy Rental
+    // przelecieć po tablicy z obiektami lokatorów i wyciągnąć czynsz z każdego lokatora
+    // return (koszt spółdzielni - rachunki) + suma czynszów
+  }
+}
+
+class Rental {
+  #id;
+  #tenants = [];
+  #rentTotal;
+
+  constructor(
+    housingAssociationCost,
+    bills,
+    roomsNumber,
+    floor,
+    zipCode,
+    city,
+    street,
+    apartamentNumber,
+    title,
+    area,
+    deposit,
+    mail,
+    phoneNumber,
+    facilities
+  ) {
+    this.housingAssociationCost = housingAssociationCost;
+    this.bills = bills;
+    this.roomsNumber = roomsNumber;
+    this.floor = floor;
+    this.zipCode = zipCode;
+    this.city = city;
+    this.street = street;
+    this.apartamentNumber = apartamentNumber;
+    this.title = title;
+    this.area = area;
+    this.deposit = deposit;
+    this.mail = mail;
+    this.phoneNumber = phoneNumber;
+    this.facilities = facilities;
+  }
+
+  _addNewTenant() {
+    // let tenant;
+    // this.#tenants.push(tenant);
+  }
+
+  _deleteTenant() {}
+
+  _calculateTotalRent() {}
+}
+
+class Apartament extends Rental {}
+class Room extends Rental {}
+class House extends Rental {}
