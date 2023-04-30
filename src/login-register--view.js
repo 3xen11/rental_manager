e0737a18e57aa33dbb9c69e275890f40038c0506
Dawid Dardziński <dardzinski.d@gmail.com>
2023-04-30 22:45:36 +0200

    project [changed,refactored]: changed styles and refactor code project for tailwind

--- src/login-register--view.js
+++ src/login-register--view.js
@@ -1,66 +1,86 @@
 const loginSide = document.querySelector(".login__container");
 const registerSide = document.querySelector(".register__container");
-
 const loginForm = document.querySelector(".login-form");
 const registerForm = document.querySelector(".register-form");
-
 const loginSwitch = document.querySelector(".login-switch");
-const loginSwitchSmall = document.querySelector(".login-switch--small");
-const registerSwitchSmall = document.querySelector(".register-switch--small");
+const loginSwitchMobile = document.querySelector(".login-switch--mobile");
 const registerSwitch = document.querySelector(".register-switch");
-
+const registerSwitchMobile = document.querySelector(".register-switch--mobile");
 const loginSticker = document.querySelector(".login-sticker");
 const registerSticker = document.querySelector(".register-sticker");
-
 const loginSwtichContainer = document.querySelector(".login-switch__container");
 const registerSwtichContainer = document.querySelector(
   ".register-switch__container"
 );
+const registerSwtichMobileContainer = document.querySelector(
+  ".register-switch__mobile-container"
+);
+const loginSwtichMobileContainer = document.querySelector(
+  ".login-switch__mobile-container"
+);
 
-const viewLoginSwitch = () => {
-  loginSide.classList.add("login-background--gradient");
-  loginSticker.classList.remove("image-big");
-  loginSwtichContainer.classList.add("hide");
-  loginForm.classList.remove("hide");
-  registerSide.classList.remove("register-background--gradient");
-  registerSticker.classList.add("image-big");
-  registerSwtichContainer.classList.remove("hide");
-  registerForm.classList.add("hide");
-};
+// const viewportWidth = window.innerWidth;
 
-loginSwitch.addEventListener("click", viewLoginSwitch);
+class SignView {
+  viewportWidth = window.innerWidth;
 
-const viewRegisterSwitch = () => {
-  loginSide.classList.remove("login-background--gradient");
-  loginSticker.classList.add("image-big");
-  loginSwtichContainer.classList.remove("hide");
-  loginForm.classList.add("hide");
-  registerSide.classList.add("register-background--gradient");
-  registerSticker.classList.remove("image-big");
-  registerSwtichContainer.classList.add("hide");
-  registerForm.classList.remove("hide");
-};
+  constructor() {
+    this.bigImageMediumWidth();
 
-registerSwitch.addEventListener("click", viewRegisterSwitch);
+    loginSwitch.addEventListener("click", this.viewLoginSwitch.bind(this));
+    registerSwitch.addEventListener(
+      "click",
+      this.viewRegisterSwitch.bind(this)
+    );
+    loginSwitchMobile.addEventListener(
+      "click",
+      this.viewLoginSwitch.bind(this)
+    );
+    registerSwitchMobile.addEventListener(
+      "click",
+      this.viewRegisterSwitch.bind(this)
+    );
+  }
 
-const viewLoginSwitchSmall = () => {
-  loginSide.classList.remove("d-none");
-  registerSide.classList.add("d-none");
-  loginSide.classList.add("login-background--gradient");
-  loginSticker.classList.remove("image-big");
-  loginSwtichContainer.classList.add("hide");
-  loginForm.classList.remove("hide");
-};
+  bigImageMediumWidth() {
+    if (this.viewportWidth >= 768 && !loginForm.classList.contains("hidden")) {
+      loginSticker.classList.remove("img-big");
+      registerSticker.classList.add("img-big");
+    }
 
-loginSwitchSmall.addEventListener("click", viewLoginSwitchSmall);
+    if (this.viewportWidth >= 768 && loginForm.classList.contains("hidden")) {
+      loginSticker.classList.add("img-big");
+      registerSticker.classList.remove("img-big");
+    }
+  }
 
-const viewRegisterSwitchSmall = () => {
-  loginSide.classList.add("d-none");
-  registerSide.classList.remove("d-none");
-  loginSide.classList.remove("login-background--gradient");
-  loginSticker.classList.add("image-big");
-  loginSwtichContainer.classList.remove("hide");
-  loginForm.classList.add("hide");
-};
+  viewLoginSwitch() {
+    registerSide.classList.add("max-md:hidden");
+    registerForm.classList.add("hidden");
+    registerSwtichContainer.classList.remove("hidden");
+    loginSwtichContainer.classList.add("hidden");
+    loginForm.classList.add("flex");
+    loginForm.classList.remove("hidden");
+    loginSwtichMobileContainer.classList.remove("md:hidden");
+    loginSwtichMobileContainer.classList.add("md:flex");
+    loginSide.classList.remove("hidden");
+    loginSide.classList.add("flex");
+    this.bigImageMediumWidth();
+  }
 
-registerSwitchSmall.addEventListener("click", viewRegisterSwitchSmall);
+  viewRegisterSwitch() {
+    registerSide.classList.remove("max-md:hidden");
+    registerForm.classList.remove("hidden");
+    registerSwtichContainer.classList.add("hidden");
+    loginSwtichContainer.classList.remove("hidden");
+    loginForm.classList.remove("flex");
+    loginForm.classList.add("hidden");
+    loginSwtichMobileContainer.classList.add("md:hidden");
+    loginSwtichMobileContainer.classList.remove("md:flex");
+    loginSide.classList.add("hidden");
+    loginSide.classList.remove("flex");
+    this.bigImageMediumWidth();
+  }
+}
+
+const signView = new SignView();
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "375px",
+      ssm: "542px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
